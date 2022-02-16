1612fe09ff5081f4d0d0f98cfb34ec7623cd721a
Wasim Ishaq Khan <wasimwik919@gmail.com>
2022-02-16 22:49:17 +0530

    First commit

---
+++ src/test/java/com/wasim919/translator/TranslatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wasim919.translator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TranslatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
