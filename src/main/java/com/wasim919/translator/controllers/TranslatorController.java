08a4fa3e03080bec6ce9b91fbec917950edbb0f4
Wasim Ishaq Khan <wasimwik919@gmail.com>
2022-02-19 00:42:08 +0530

    done

--- src/main/java/com/wasim919/translator/controllers/TranslatorController.java
+++ src/main/java/com/wasim919/translator/controllers/TranslatorController.java
@@ -7,6 +7,11 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
+/**
+ * @author  Wasim
+ * @version 1.0.0
+ * It has 2 end points that translate text from english to spanish and vice-versa
+ */
 @RestController
 @RequestMapping("/translate")
 public class TranslatorController {
@@ -17,14 +22,29 @@ public class TranslatorController {
         this.translatorService = translatorService;
     }
 
-    @GetMapping("/greet")
-    public String greet() {
-        return "Hello";
-    }
-
+    /**
+     * This method returns the translation of input english test into spanish
+     * @param   text      text from english language that you want to translate to spanish
+     * @return  text      translation of the text into spanish
+     *
+     * I have accepted the text-to-be-translated as a body argument for privacy reasons and not
+     * making the request too long (by using query params)
+     */
     @PostMapping("/translateFromEnToEs")
     public String translateFromEnToEs(@RequestBody @NotNull @NotBlank String text) {
         return this.translatorService.translateFromEnToEs(text);
     }
 
+    /**
+     * This method returns the translation of input spanish test into english
+     * @param   text      text from spanish language that you want to translate to english
+     * @return  text      translation of the text into english
+     *
+     * I have accepted the text-to-be-translated as a body argument for privacy reasons and not
+     * making the request too long (by using query params)
+     */
+    @PostMapping("/translateFromEsToEn")
+    public String translateFromEsToEn(@RequestBody @NotNull @NotBlank String text) {
+        return this.translatorService.translateFromEsToEn(text);
+    }
 }
--- src/main/java/com/wasim919/translator/services/TranslatorService.java
+++ src/main/java/com/wasim919/translator/services/TranslatorService.java
@@ -1,7 +1,10 @@
 package com.wasim919.translator.services;
 
+import java.util.Optional;
 import com.wasim919.translator.entities.TranslatedEntity;
 import com.wasim919.translator.repositories.TranslatorRepository;
+import org.json.JSONObject;
+import org.springframework.data.domain.Example;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -9,36 +12,59 @@ import org.springframework.web.client.RestTemplate;
 
 @Service
 public class TranslatorService {
-    private final TranslatorRepository translatorRepository;
-    private final RestTemplate restTemplate;
+	private final TranslatorRepository translatorRepository;
+	private final RestTemplate restTemplate;
 
-    private final String translateApiUri = "https://libretranslate.de/translate";
+	private final String translateApiUri = "https://libretranslate.de/translate";
 
-    @Autowired
-    public TranslatorService(TranslatorRepository translatorRepository, RestTemplate restTemplate) {
-        this.translatorRepository = translatorRepository;
-        this.restTemplate = restTemplate;
-    }
+	@Autowired
+	public TranslatorService(TranslatorRepository translatorRepository, RestTemplate restTemplate) {
+		this.translatorRepository = translatorRepository;
+		this.restTemplate = restTemplate;
+	}
 
-    public String translateFromEnToEs(String text) {
-            TranslatedEntity translatedEntity = TranslatedEntity.builder()
-                    .q(text)
-                    .source("en")
-                    .target("es")
-                    .build();
-            ResponseEntity<TranslatedEntity> response = restTemplate.postForEntity(translateApiUri, translatedEntity, TranslatedEntity.class);
-            System.out.println(response.getBody());
-            return response.getBody().getTranslatedText();
-    }
+	public String translateFromEnToEs(String text) {
+		try {
+			TranslatedEntity translatedEntity = TranslatedEntity.builder()
+					.q(text)
+					.source("en")
+					.target("es")
+					.build();
+			return getTranslatedText(translatedEntity);
+		} catch(Exception e) {
+			return e.getMessage();
+		}
+	}
 
-    public String translateFromEsToEn(String text) {
-            TranslatedEntity translatedEntity = TranslatedEntity.builder()
-                    .q(text)
-                    .source("es")
-                    .target("en")
-                    .build();
-            ResponseEntity<TranslatedEntity> response = restTemplate.postForEntity(translateApiUri, translatedEntity, TranslatedEntity.class);
-            System.out.println(response.getBody());
-            return response.getBody().getTranslatedText();
-    }
+	public String translateFromEsToEn(String text) {
+		try {
+			TranslatedEntity translatedEntity = TranslatedEntity.builder()
+					.q(text)
+					.source("es")
+					.target("en")
+					.build();
+			return getTranslatedText(translatedEntity);
+		} catch(Exception e) {
+			return e.getMessage();
+		}
+	}
+
+	private String getTranslatedText(TranslatedEntity translatedEntity) {
+		Optional<TranslatedEntity> result = this.translatorRepository.findOne(Example.of(translatedEntity));
+
+		if (result.isPresent()) {
+			return result.get().getTranslatedText();
+		}
+		ResponseEntity<TranslatedEntity> response = restTemplate.postForEntity(translateApiUri, translatedEntity,
+				TranslatedEntity.class);
+		if (!response.hasBody()) {
+			return "No translation found";
+		}
+		if (response.getStatusCodeValue() == 200 && response.hasBody()) {
+			JSONObject jsonObject = new JSONObject(response.getBody());
+			translatedEntity.setTranslatedText(jsonObject.getString("translatedText"));
+			this.translatorRepository.save(translatedEntity);
+		}
+		return translatedEntity.getTranslatedText();
+	}
 }
